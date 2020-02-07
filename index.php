00c1f3712ceffac6e7fd435922e58745bcd8eea6
i-yard <iyardsev@gmail.com>
2020-02-07 16:47:32 +0300

    update common interface

--- index.php
+++ index.php
@@ -8,12 +8,17 @@ if (file_exists($autoloadPath1)) {
     require_once $autoloadPath2;
 }
 
+use App\Log;
+use App\formatters\WeatherFormatter;
 use App\YandexWheather;
 use GuzzleHttp\Client;
 
-
 $httpClient = new Client();
 $yandexWheather = new YandexWheather($httpClient);
-$weather = $yandexWheather->get('xml');
-Log::file($weather);
+$weather = $yandexWheather->get();
+
+$whetherFormatter = WeatherFormatter::getInstance('xml');
+$weatherFormatted = $whetherFormatter->format($weather);
+
+Log::save($weatherFormatted);
 
